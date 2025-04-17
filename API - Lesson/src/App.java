908e3ecb7ff56710e2adc828c5f792cbd50a8bc0
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-04-16 22:05:26 -0400

    Update App.java
    beginner comments and lesson goals added

--- API - Lesson/src/App.java
+++ API - Lesson/src/App.java
@@ -1,4 +1,11 @@
+/* API lesson where i will eventually create an API - probably weather but i also want to make  */
+
+//imports
+
+// Main class - App
 public class App {
+
+    //main method
     public static void main(String[] args) throws Exception {
         
     }
